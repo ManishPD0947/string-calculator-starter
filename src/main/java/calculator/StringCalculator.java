039330440aab4a74736d16def160ab79202eef25
Manish Kumar Sahu <manishsahu4656@gmail.com>
2021-10-17 04:27:27 +0530

    Exception handling for negative values

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,28 +1,47 @@
 package calculator;
 
-class StringCalculator {
-	private final static String delimeter = "[.*-,\n;/%&^#@]+";
-    
-	public int add(String input) {
-        String [] numArray=input.split(delimeter);
-		
-        if(input.isEmpty())return 0;//return 0 if string is empty
-		
-        if(numArray.length==1) return Integer.parseInt(input);//if single number is present returns integer value
-		
-        else return addAllNumbers(numArray);// if multiple numbers are present, sum of all the numbers are returned
-		
-	}
+import java.util.ArrayList;
+import java.util.List;
 
-	//Method to find sum of all the numbers in a string
-	private int addAllNumbers(String[] num) {
+class StringCalculator {
+	
+	public int add(String input) throws Exception {
 		int sum=0;
-	  
-    	for( int i = 0; i <= num.length - 1; i++)
+		int flag=0;
+		List<Integer>negativeNumbers=new ArrayList<>();									//List for storing negative values
+		String [] numArray=input.split("[*,\n;/%&^#@]+");
+        
+        
+        if("".equals(input)||input==null)return 0;													//return 0 if string is empty
+		
+        if(numArray.length==1) return Integer.parseInt(input);							//if single number is present returns integer value
+		
+		
+		for( int i = 0; i <= numArray.length - 1; i++)
     	{
-    		 sum = sum+Integer.parseInt(num[i]);
+    		if(Integer.parseInt(numArray[i])<0) {
+    			flag=1;
+    			negativeNumbers.add(Integer.parseInt(numArray[i]));							//Adding negative values in a list for printing in exception message
+    		}
+    		 if(flag!=1) {
+    			 sum = sum+Integer.parseInt(numArray[i]);
+    		 }
     	}
-    	return sum;
+		
+	    if(flag==1) {
+	    	throw new Exception("negatives not allowed "+negativeNumbers);	//check for multiple negative values and show them as exception message
+	    }
+    	
+	    return sum;
+	}
+	
+	public static void main(String[] args) {
+		StringCalculator stringCalculator=new StringCalculator();
+		try {
+			System.out.println("Output : "+stringCalculator.add("4**5,\n6"));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -7,30 +7,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringCalculatorShould {
 	
     @Test
-    public void empty_string_should_return_0() {
+    public void empty_string_should_return_0() throws Exception{
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    public void string_with_single_number_should_return_number_as_int() {
+    public void string_with_single_number_should_return_number_as_int()throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
     
     @Test
-    public void whenMoreThanTwoNumbersAreUsed_ReturnValueIsTheSum() {
+    public void whenMoreThanTwoNumbersAreUsed_ReturnValueIsTheSum()throws Exception {
     	StringCalculator stringCalculator = new StringCalculator();
     	assertEquals(16,stringCalculator.add("3,3,10"));
     }
     @Test
-    public void handleNewLinesBetweenNumbers_ReturnValueIsTheSum() {
+    public void handleNewLinesBetweenNumbers_ReturnValueIsTheSum()throws Exception {
     	StringCalculator stringCalculator = new StringCalculator();
     	assertEquals(9,stringCalculator.add("2\n4,3"));
     }
     @Test
-    public void multipleDelimiter_ReturnSum() {
+    public void multipleDelimiter_ReturnSum() throws Exception {
     	StringCalculator stringCalculator = new StringCalculator();
     	assertEquals(15,stringCalculator.add("2//.4#3&6"));
     }
+    @Test
+    public void  negativeInputReturnsException() throws Exception {
+    	StringCalculator stringCalculator = new StringCalculator();
+    	//stringCalculator.add("-4");
+    	stringCalculator.add("-4,-9");
+    }
+    
 }
